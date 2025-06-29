59a93a427a5fa71b5fb7001f53a677e600d86f45
deepesh-sr <220714100078@cutm.ac.in>
2025-06-29 23:51:35 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Order from './pages/Order'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/product/:prouctId' element={<Product />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+     
     </div>
   )
 }
