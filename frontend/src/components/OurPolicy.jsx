4547fb9e20b71989e45e1c96de4ca8df36d7e993
Deepesh Singh Rathore <deepeshsinghrathore@Deepeshs-MacBook-Air.local>
2025-08-09 15:03:45 +0530

    policy component added

---
+++ frontend/src/components/OurPolicy.jsx
@@ -0,0 +1,29 @@
+const OurPolicy = ()=>{
+    return(
+        
+        <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-4" >
+
+            <div>
+                <img src="frontend/src/assets/user-solid.svg" alt="n" className="w-12 m-auto mb-5"/>
+                <p className="font-semibold">Easy Exchange Policy</p>
+                <p className="text-gray-400">We offer Hassle free exchange policy</p>
+             </div>
+             
+            <div>
+                <img src="frontend/src/assets/circle-check-solid-full.svg" alt="n" className="w-12 m-auto mb-5"/>
+                <p className="font-semibold">Easy Exchange Policy</p>
+                <p className="text-gray-400">We offer Hassle free exchange policy</p>
+             </div>
+             
+            <div>
+                <img src="frontend/src/assets/circle-check-solid-full.svg" alt="n" className="w-12 m-auto mb-5"/>
+                <p className="font-semibold">Easy Exchange Policy</p>
+                <p className="text-gray-400">We offer Hassle free exchange policy</p>
+             </div>
+             
+        </div>
+        
+    )
+}
+
+export default OurPolicy;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection/>
+      <OurPolicy/>
     </div>
   )
 }
